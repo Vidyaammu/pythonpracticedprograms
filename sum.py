dafe00f7f626cb213224b3a2b4566f776b025944
Vidyaammu <105410423+Vidyaammu@users.noreply.github.com>
2023-03-02 12:25:25 +0530

    To find Sum in given range

---
+++ sum.py
@@ -0,0 +1,15 @@
+#formula : Sum = b * ( b + 1 ) / 2 – a * ( a + 1 ) / 2 + a
+
+'''
+num1 = 10
+num2 = 35
+sum = 0
+for i in range(num1,num2+1):
+    sum+=i
+print(sum)
+'''
+
+num1 = 12
+num2 = 45
+print(num2*(num2+1)/2 - num1*(num1+1)/2+num1)
+
