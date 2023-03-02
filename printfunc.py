b512030642f4abd2a9458dba94bef7ee8554a59f
Vidyaammu <105410423+Vidyaammu@users.noreply.github.com>
2023-03-02 12:15:18 +0530

    using print function

---
+++ printfunc.py
@@ -0,0 +1,15 @@
+'''
+print('A','B','C')
+print('a','b',sep=" ")
+print('a','b')
+print('a','b',sep="")
+print('12','12',sep='-',end="$")
+print("Hello World")
+xyz=23
+print(xyz)
+'''
+
+import dis
+def examp():
+    print("Hello World")
+    dis.dis(examp)
