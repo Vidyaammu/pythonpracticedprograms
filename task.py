2e0c91ac945927fad18085717d9d09265c66f427
Vidyaammu <105410423+Vidyaammu@users.noreply.github.com>
2023-03-02 12:35:16 +0530

    Task on 24-01-2023

---
+++ task.py
@@ -0,0 +1,75 @@
+#Area and perimeter of the rectangle
+'''
+length=int(input("enter value of length"))
+bredth=int(input("enter value of bredth"))
+area=length*bredth
+res=length+bredth
+perimeter=2*res
+print("area of rectangle", area)
+print("perimeter of rectangle",perimeter)
+
+l=10
+b=2
+a=l*b
+p=2*(l+b)
+print("area is",a)
+print("perimeter is",p)
+
+
+#Area of triangle
+
+base=float(input("enter base value"))
+height=int(input("enter height value"))
+area=0.5*(base*height)
+print(area)
+
+b=0.4
+h=5
+a=0.5*(b*h)
+print("area of triangle",a)
+
+#Simple Interest SI=p*T*R/100
+
+p=int(input("enter value of principle"))
+t=int(input("enter value of Time"))
+r=int(input("enter value of Rate"))
+si=(p*t*r)/100
+print("simple interest is",si)
+
+
+
+#Finding celcius  c= 5/9*(f-32)
+
+f=int(input("enter value"))
+celcius=5/9*(f-32)
+print(round(celcius))
+'''
+
+#Mathematical operaions
+
+a=10
+b=8.5
+
+#addition=a+b
+#subtraction=a-b
+#product=a*b
+#division=a/b
+
+print(a+b," is addition value \n", a-b,"is subtraction value \n", a*b,"is product value \n",a/b,"is division value\n")
+
+'''
+#Marks Average
+
+telugu=98
+hindi=98
+english=90
+maths=100
+science=89
+social=79
+total=telugu+hindi+english+maths+science+social
+average=total/6
+print("Average of total marks",average)
+'''
+
+
+
