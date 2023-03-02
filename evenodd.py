08f12bf3121bb49d36fd2a3398ee87b8b1be6cd8
vidya <vidyalathav@gmail.com>
2023-03-02 12:05:09 +0530

    EvenOdd

---
+++ evenodd.py
@@ -0,0 +1,37 @@
+'''
+num=int(input("enter a number"))
+if num % 2 == 0:
+    print("given number is Even")
+else:
+    print("given number is odd")
+'''
+
+#num=20
+#print("even") if num % 2 == 0 else print("odd")
+
+'''
+def EvenOdd():
+    num = int(input("enter a number"))
+    if num % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+EvenOdd()
+'''
+
+class evenodd:
+    def __init__(self):
+        print("example")
+    def even():
+        num = 20
+        if num % 2 == 0:
+            print("given is Even")
+    def odd():
+        num = 31
+        if num % 2 != 0:
+            print("given is odd")
+
+evenodd.even()
+evenodd.odd()
+
+
