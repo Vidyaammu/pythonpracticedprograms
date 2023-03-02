23a9ac608d8fd7bc7694604fd4d05012d24524ca
Vidyaammu <105410423+Vidyaammu@users.noreply.github.com>
2023-03-02 12:27:32 +0530

    Sum of N natural numbers

---
+++ sumNnatural.py
@@ -0,0 +1,12 @@
+# formula for sum of n natural numbers Sum = ( Num * ( Num + 1 ) ) / 2
+
+
+num = 80
+sum = 0
+for i in range(num+1):
+    sum+=i
+print(sum)
+
+
+#num = 80
+#print((num*(num+1))/2)
