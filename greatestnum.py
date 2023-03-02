c5c4010d420226edf4765b40eb19bebd3b0247f2
Vidyaammu <105410423+Vidyaammu@users.noreply.github.com>
2023-03-02 12:10:31 +0530

    Greatest Number

---
+++ greatestnum.py
@@ -0,0 +1,13 @@
+'''
+num1,num2 = 39,58
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)
+'''
+
+#num1, num2 = 6,38
+#print(num1 if num1 > num2 else num2)
+
+num1, num2,num3 =45,78,90
+print(max(num1,num2,num3))
