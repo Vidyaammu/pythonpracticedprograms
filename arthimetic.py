8a40633f1eff961a75bfeacb14880a8a769bbd5a
Vidyaammu <105410423+Vidyaammu@users.noreply.github.com>
2023-03-08 00:34:15 +0530

    ArithmeticOperators

---
+++ arthimetic.py
@@ -0,0 +1,50 @@
+# Arithmetic operators
+'''
+a = 4
+b = 8
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a//b)
+print(a**b)
+'''
+
+a=7
+b=5
+
+# Addition
+result = a + b
+print("The sum of a and b is:", result)
+
+# Subtraction
+result = a - b
+print("The difference of a and b is:", result)
+
+# Multiplication
+result = a * b
+print("The product of a and b is:", result)
+
+# Division
+result = a / b
+print("The division of a and b is:", result)
+
+# Modulus
+result = a % b
+print("The remainder when a is divided by b is:", result)
+
+
+
+
+
+
+
+
+# Bitwise operators
+a = 60
+b = 13
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
